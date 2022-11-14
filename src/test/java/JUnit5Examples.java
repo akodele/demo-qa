e305ad93c41c70e813a0c332dc1fc97e34596914
akalibekova <dairako2000@gmail.com>
2022-11-14 17:00:06 +0600

    added JUnit5

---
+++ src/test/java/JUnit5Examples.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JUnit5Examples {
+    @Test
+    void firstTest(){
+        System.out.println("firstTest()!!!!!");
+        assertTrue(3<2);
+    }
+}
